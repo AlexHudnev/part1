439f6796000b76b3adf7088e6ccb9126fc8ac417
AlexHudnev <hudneval@mail.ru>
2018-11-07 17:41:03 +0300

    Update prog1.rb

--- prog1.rb
+++ prog1.rb
@@ -1,9 +1,8 @@
 def palindrome?(input)
-  str_tmp =  input.downcase.scan(/(\w+|[а-я]+)/).join
+  str_tmp =  input.to_s.downcase.scan(/(\w+|[а-я]+)/).join
   return str_tmp.reverse == str_tmp
 end
 puts palindrome?("A man, a plan, a canal -- Panama")
 puts palindrome?("Madam, I'm Adam!")
 puts palindrome?(333)
-puts palindrome?(nil)
 puts palindrome?("Abracadabra")
